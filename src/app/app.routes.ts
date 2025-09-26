a8b351f2e9c938fb0bbe1a6d764b5fe37eb2ce27
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:29:50 -0300

    feat: Add initial routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductDetailsComponent } from './features/products/components/product-details/product-details.component';
+import { HomeFeature } from './features/home/pages/home/home.feature';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: HomeFeature },
+   { path: 'products/details/:id', component: ProductDetailsComponent },
+   { path: '**', redirectTo: '' },
+];
