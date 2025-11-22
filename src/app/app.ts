16569b0d0ce51867e0fe190b5cadb1b30e8efb5b
Wesley00s <wesley300rodrigues@gmail.com>
2025-11-22 20:03:21 -0300

    refactor: Integrate Toast service and component into app layout for enhanced user notifications

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { Observable } from 'rxjs';
+import { Toast } from './core/@types/Toast';
+import { AsyncPipe } from '@angular/common';
+import { ToastComponent } from './shared/components/toast/toast.component';
+import { ToastService } from './core/services/toast.service';
 
 @Component({
    standalone: true,
    selector: 'app-root',
-   imports: [RouterOutlet, NavbarComponent],
+   imports: [RouterOutlet, NavbarComponent, AsyncPipe, ToastComponent],
    templateUrl: './app.html',
    styleUrl: './app.sass',
 })
 export class App {
    protected title = 'Ecommerce-Front';
+   private toastService = inject(ToastService);
+
+   toasts$: Observable<Toast[]> = this.toastService.getToasts();
 }
