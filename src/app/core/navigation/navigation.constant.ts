3c86c5911fdb2a69da06112c3b5e545e01c4c495
Wesley00s <wesley300rodrigues@gmail.com>
2025-11-30 00:13:18 -0300

    feat: Update navigation path from 'about' to 'category'

--- src/app/core/navigation/navigation.constant.ts
+++ src/app/core/navigation/navigation.constant.ts
@@ -1,5 +1,5 @@
 export const NAV_PATHS: { path: string; label: string }[] = [
    { path: '/', label: 'Home' },
    { path: '/shop', label: 'Shop' },
-   { path: '/about', label: 'Sobre' },
+   { path: '/category', label: 'Categorias' },
 ];
