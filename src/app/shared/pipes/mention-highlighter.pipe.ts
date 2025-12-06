dc3719f78fac548b0d5bd07168bbf556f2debae1
Wesley00s <wesley300rodrigues@gmail.com>
2025-12-06 20:01:19 -0300

    refactor: Enhance Mention Regex

--- src/app/shared/pipes/mention-highlighter.pipe.ts
+++ src/app/shared/pipes/mention-highlighter.pipe.ts
@@ -6,7 +6,7 @@ export class MentionHighlighterPipe implements PipeTransform {
    protected sanitizer = inject(DomSanitizer);
 
    transform(value: string): SafeHtml {
-      const mentionRegex = /(@\w+)/g;
+      const mentionRegex = /(@[\p{L}\w]+)/gu;
       const highlightedText = value.replace(
          mentionRegex,
          `<strong class="text-orange-600">$1</strong>`,
