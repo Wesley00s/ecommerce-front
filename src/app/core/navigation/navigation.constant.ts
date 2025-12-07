2457697e4e99800e8a120dfb32c80366313c5e22
Wesley00s <wesley300rodrigues@gmail.com>
2025-12-07 03:09:15 -0300

    refactor: Remove 'Home' link from navigation

--- src/app/core/navigation/navigation.constant.ts
+++ src/app/core/navigation/navigation.constant.ts
@@ -1,5 +1,4 @@
 export const NAV_PATHS: { path: string; label: string }[] = [
-   { path: '/', label: 'Home' },
    { path: '/shop', label: 'Shop' },
    { path: '/category', label: 'Categorias' },
 ];
